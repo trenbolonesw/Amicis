66001cf062eac5d6b0c95395c28abaa43312dbf9
bro <107288822+trenbolonesw@users.noreply.github.com>
2024-10-28 13:59:39 -0500

    changed image, ajusted body padding

---
+++ app/Home/page.js
@@ -0,0 +1,19 @@
+import classes from './bigpage.module.css'
+export default function AmicisAbout(){
+    return(
+       
+        <main className={classes.page}>
+            <div>
+            <p>
+            welcome to Amicis!
+            </p>
+            <p>
+            I hope you enjoy this little social media web demo.
+            </p>
+            </div>
+            
+           
+        </main>
+        
+    )
+}
